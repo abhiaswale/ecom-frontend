5fe7c69c4619530f65ad31367234e4d52f196c0d
abhiaswale <aswaleabhishek8@gmail.com>
2022-05-24 11:36:22 +0530

    added search bar

--- src/components/Navigation/SearchBar.js
+++ src/components/Navigation/SearchBar.js
@@ -9,25 +9,58 @@ const SearchBar = () => {
   const changeHandler = (e) => {
     let array = [];
     setSearchItem(e.target.value);
-
     if (searchItem.length >= 2) {
       array = suggestions.filter((item) => {
         return item.toLowerCase().startsWith(searchItem.toLowerCase());
       });
     }
-    console.log(array);
     setSuggest(array);
+  };
+
+  const getSuggestions = () => {
+    if (suggest.length === 0) {
+      return;
+    }
+    return (
+      <ul>
+        {suggest &&
+          suggest.map((item) => (
+            <li
+              className="cursor-pointer"
+              onClick={() => {
+                setSearchItem(item);
+                submitHandler();
+                setSuggest([]);
+              }}
+            >
+              {item}
+            </li>
+          ))}
+      </ul>
+    );
+  };
+
+  const submitHandler = (e) => {
+    // e.preventDefault();
+    console.log(searchItem);
+    fetch(`http://localhost:3000/search?searchTerm=${searchItem}`, {
+      method: "GET",
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
   };
 
   return (
     <>
-      <input type="text" onChange={changeHandler} value={searchItem}></input>
-      <ul>
-        {suggest &&
-          suggest.map((i) => {
-            <li>{i}</li>;
-          })}
-      </ul>
+      <form onSubmit={submitHandler}>
+        <input type="text" onChange={changeHandler} value={searchItem}></input>
+        {searchItem && getSuggestions()}
+        <button type="submit">Search</button>
+      </form>
     </>
   );
 };
