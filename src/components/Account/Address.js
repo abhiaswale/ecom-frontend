6ab3e28186a0549eab0509cfe656b8b664e6eebc
abhiaswale <aswaleabhishek8@gmail.com>
2022-05-25 11:05:09 +0530

    Added addresses

--- src/components/Account/Address.js
+++ src/components/Account/Address.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { useLocation } from "react-router-dom";
+import AuthContext from "../Context/auth-context";
 
 const Address = () => {
+  const authCtx = useContext(AuthContext);
   const location = useLocation();
-  const userData = location.state;
-  console.log(userData);
-  return <div></div>;
+  const userData = location.state.details;
+  console.log(userData.user.addresses);
+  const [addresses, setAddresses] = useState(userData.user.addresses);
+
+  const addAddress = () => {
+    fetch("http://localhost:3000/user/add-address", {
+      method: "POST",
+      headers: {
+        Authorization: authCtx.token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({}),
+    });
+  };
+  return (
+    <div>
+      {addresses.map((i) => (
+        <p>{i.Name}</p>
+      ))}
+    </div>
+  );
 };
 
 export default Address;
