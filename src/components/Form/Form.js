91686316ac72d52557c4647d4ede6cccac077b9f
abhiaswale <aswaleabhishek8@gmail.com>
2022-06-14 12:14:59 +0530

    Made nav bar responsive

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,7 +3,7 @@ import React from "react";
 const Form = (props) => {
   return (
     <div className="flex justify-center items-center h-auto my-[5rem]">
-      <div className="flex justify-center items-center flex-col w-2/5 text-sm h-auto  p-4">
+      <div className="flex justify-center items-center flex-col w-full lg:w-2/5 text-sm h-auto  p-4">
         {props.children}
       </div>
     </div>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { NavLink, Link } from "react-router-dom";
 import AuthContext from "../Context/auth-context";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
@@ -8,26 +8,59 @@ import Badge from "@mui/material/Badge";
 import CartContext from "../Context/cart-context";
 import EComLogo from "../Assets/L1.png";
 import SearchBar from "./SearchBar";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import { MobileNavlinks } from "./NavLinks";
 
 const Navigation = () => {
   const authCtx = useContext(AuthContext);
   const cartContext = useContext(CartContext);
   const [count, setCount] = useState(0);
   const [wCount, setWCount] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+  let menuRef = useRef();
 
   useEffect(() => {
-    console.log("called ctx");
+    let handler = (e) => {
+      if (!menuRef.current.contains(e.target)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
+
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  });
+
+  useEffect(() => {
     setCount(cartContext.cartQuantity);
     setWCount(cartContext.wishlistQuantity);
   });
 
-  const logoutHandler = () => {
-    authCtx.logout();
-  };
   return (
-    <div>
-      <div className="h-16 bg-[#0E3EDA] flex justify-around items-center ">
-        <nav className="flex justify-center items-center">
+    <div className="h-auto w-screen">
+      <div className="px-3 h-16 bg-[#0E3EDA] flex justify-between lg:justify-around items-center ">
+        <div
+          className="lg:hidden flex justify-center items-center mx-1 "
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+        >
+          {isOpen ? (
+            <CloseIcon style={{ fontSize: "2.1rem" }} />
+          ) : (
+            <MenuIcon style={{ fontSize: "2.1rem" }} />
+          )}
+
+          <h2 className="mx-3">ECOM</h2>
+        </div>
+
+        <div ref={menuRef} className="absolute z-40 top-0 left-0 w-3/5">
+          {isOpen && <MobileNavlinks setIsOpen={setIsOpen} />}
+        </div>
+
+        <nav className="hidden lg:flex justify-center items-center">
           <Link to="">
             <img src={EComLogo} className="h-10 w-32"></img>
           </Link>
@@ -47,12 +80,33 @@ const Navigation = () => {
           </NavLink>
         </nav>
 
-        <div className="w-96">
+        <div className="hidden lg:block w-96">
           <SearchBar />
         </div>
+        {/* //Mobile Navlinks */}
 
-        <div className="flex justify-evenly items-center flex-row w-72">
-          {/* {authCtx.isAuth && <button onClick={logoutHandler}>Logout</button>} */}
+        <div className="lg:hidden flex justify-center items-center ">
+          <Badge
+            badgeContent={authCtx.isAuth ? wCount : 0}
+            color="primary"
+            className="mx-2"
+          >
+            <Link to="/wishlist">
+              <FavoriteIcon />
+            </Link>
+          </Badge>
+          <Badge
+            badgeContent={authCtx.isAuth ? count : 0}
+            color="primary"
+            className="mx-2"
+          >
+            <Link to="/cart">
+              <ShoppingCartIcon />
+            </Link>
+          </Badge>
+        </div>
+
+        <div className="hidden lg:flex justify-evenly items-center flex-row w-72">
           <Link to="/account" className="text-lg">
             <AccountCircleIcon className="text-lg" />
             {authCtx.isAuth && <p className="text-sm">Hi,{authCtx.userName}</p>}
@@ -71,6 +125,9 @@ const Navigation = () => {
             </Link>
           </Badge>
         </div>
+      </div>
+      <div className="lg:hidden p-1  ">
+        <SearchBar />
       </div>
     </div>
   );
